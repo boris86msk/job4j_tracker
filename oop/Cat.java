c3acb006822344ca9214e199bd7131f6b5faa0dd
User <boris86msk@yandex.ru>
2022-04-01 21:08:15 +0300

    oop. class Dog

---
+++ oop/Cat.java
@@ -0,0 +1,7 @@
+package job4j_tracker.oop;
+
+public class Cat {
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+    }
+}
---
+++ oop/Dog.java
@@ -0,0 +1,9 @@
+package job4j_tracker.oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
