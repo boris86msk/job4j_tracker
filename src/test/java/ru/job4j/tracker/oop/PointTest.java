566feeb89fbc2e3437d0ee28f125d417373d6b54
User <boris86msk@yandex.ru>
2022-04-05 22:08:52 +0300

    distance3d

---
+++ src/test/java/ru/job4j/tracker/oop/PointTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.tracker.oop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+
+    @Test
+    public void when13to40then4() {
+        double expected = 4.24;
+        Point a = new Point(1, 3);
+        Point b = new Point(4, 0);
+        double rsl = a.distance(b);
+        Assert.assertEquals(expected, rsl, 0.01);
+    }
+
+    @Test
+    public void when020to220then3() {
+        double expected = 3.60;
+        Point a = new Point(0, 2, 0);
+        Point b = new Point(2, 2, 3);
+        double rsl = a.distance3d(b);
+        Assert.assertEquals(expected, rsl, 0.01);
+    }
+}
