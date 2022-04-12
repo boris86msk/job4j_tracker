33e536180bae55cd0dd7308b5afcd68f3afc87e4
User <boris86msk@yandex.ru>
2022-04-12 21:24:12 +0300

    Tracker.delete import

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -6,7 +6,6 @@ public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
-    private java.lang.System system;
 
     public Item add(Item item) {
         item.setId(ids++);
