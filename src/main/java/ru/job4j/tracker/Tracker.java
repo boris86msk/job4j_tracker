d5e4c915769abff654823165f1a857f1f310e19d
User <boris86msk@yandex.ru>
2022-04-12 23:00:59 +0300

    валидация исправлено по шаблону

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -47,14 +47,13 @@ public class Tracker {
     }
 
     public boolean replace(int id, Item item) {
-        boolean rsl = false;
-        if (id > 0) {
-            int index = this.indexOf(id);
+        int index = indexOf(id);
+        boolean result = index != -1;
+        if (result) {
+            item.setId(id);
             items[index] = item;
-            items[index].setId(id);
-                rsl = true;
-            }
-        return rsl;
+        }
+        return result;
     }
 
     public boolean delete(int id) {
