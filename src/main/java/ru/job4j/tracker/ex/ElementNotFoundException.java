50a1353d484c03ad98da2761f964619c288ab8d3
User <boris86msk@yandex.ru>
2022-04-24 17:44:29 +0300

    Пользовательские исключения

--- src/main/java/ru/job4j/tracker/ex/ElementNotFoundException.java
+++ src/main/java/ru/job4j/tracker/ex/ElementNotFoundException.java
@@ -1,26 +1,7 @@
 package ru.job4j.tracker.ex;
 
 public class ElementNotFoundException extends Exception {
-    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        if (key == null) {
-            throw new ElementNotFoundException();
-        }
-        int rsl = -1;
-        for (int i = 0; i < value.length; i++) {
-            if (value[i].equals(key)) {
-                rsl = i;
-                break;
-            }
-        }
-        return rsl;
-    }
-
-    public static void main(String[] args) {
-        try {
-            String[] str = {"one", "two", "three"};
-            indexOf(str, "");
-        } catch (ElementNotFoundException e) {
-            e.printStackTrace();
-        }
+    public ElementNotFoundException(String message) {
+        System.out.println(message);
     }
 }
