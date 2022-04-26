e3b3f0715fdbe0950291de6a6f5639be80ade437
User <boris86msk@yandex.ru>
2022-04-26 22:10:21 +0300

    ValidateInputTest

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.tracker;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+public class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput1() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Select:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenInValidInput2() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenInValidInput3() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"2", "1", "3"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Select:");
+        assertThat(selected, is(2));
+        selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+        selected = input.askInt("Enter menu:");
+        assertThat(selected, is(3));
+    }
+}
