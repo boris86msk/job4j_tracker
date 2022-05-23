73bd8aeab60ba19d39d5fcd1b32b3c078a7f785b
boris86msk <boris86msk@yandex.ru>
2022-05-23 12:45:05 +0300

    LexSort

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+        String[] leftNum = left.split(". ");
+        String[] rightNum = right.split(". ");
+        int i1 = Integer.parseInt(leftNum[0]);
+        int i2 = Integer.parseInt(rightNum[0]);
+        return Integer.compare(i1, i2);
+    }
+}
