b1914979ccd1102fbae8182af49103d201fc3ce2
User <boris86msk@yandex.ru>
2022-06-04 21:12:08 +0300

    FunctionalInterfaces 2

--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -19,9 +19,9 @@ public class FunctionalInterfaces {
         biCon.accept(6, "six");
         biCon.accept(7, "seven");
 
-        Predicate<Integer> biPred = (t) -> t % 2 == 0 || map.get(t).length() == 4;
+        BiPredicate<Integer, String> biPred = (i, s) -> i % 2 == 0 || s.length() == 4;
         for (Integer i : map.keySet()) {
-            if (biPred.test(i)) {
+            if (biPred.test(i, map.get(i))) {
                 System.out.println("key: " + i + " value: " + map.get(i));
             }
         }
