a58921c367751d558275aa3896d9a8d762e1a894
boris86msk <boris86msk@yandex.ru>
2022-06-07 12:26:58 +0300

    School collect

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -7,9 +7,8 @@ import java.util.stream.Collectors;
 public class School {
     public List<Student> collect(List<Student> students,
                                  Predicate<Student> predict) {
-        List<Student> rsl = students.stream()
+        return students.stream()
                 .filter(predict)
                 .collect(Collectors.toList());
-        return rsl;
     }
 }
