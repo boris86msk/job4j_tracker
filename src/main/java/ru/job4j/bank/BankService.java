ec079df410eda23842547350e95040d3c547d063
boris86msk <boris86msk@yandex.ru>
2022-06-12 14:10:50 +0300

    Optional в Stream API

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -60,7 +60,6 @@ public class BankService {
                         .stream()
                         .filter(account -> account.getRequisite().equals(requisite))
                         .findFirst());
-
     }
 
     /**
